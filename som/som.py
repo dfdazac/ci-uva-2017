8d8a3ac9f1902d2cb385323a0943f3e5681018c1
Daniel Daza <dfdazac@gmail.com>
2017-11-14 15:34:43 +0100

    Completed self-organization phase

--- som/som.py
+++ som/som.py
@@ -13,24 +13,24 @@ class SelfOrganizingMap:
         self.m = m
         self.n_units = h * w
         self.map_radius = max(h-1, w-1)/2
-        self.map = np.empty((h, w, m))
-        self.map_idx = np.array([[(i,j) for j in range(w)] for i in range(h)])
+        self.neurons = np.empty((m, h * w))
+        self.map_pos = np.array([(i,j) for i in range(h) for j in range(w)])
 
     def self_organize(self, data):
         """ Carries out self-organization in the SOM using data.
         Args:
-            - data: An N by m matrix with data points in the columns.
+            - data: An m by N matrix with data points in the columns.
         """
-        N = len(data)
+        N = data.shape[1]
         # Check if data is correct
         if N < self.n_units:
             raise ValueError("Insufficient data for self-organization")
-        if data.shape[1] != self.m:
+        if data.shape[0] != self.m:
             raise ValueError("Incorrect data dimensions")
 
-        # Initialize weights from unique samples of the data
+        # Initialize neuron weights from unique samples of the data
         indices = np.random.choice([i for i in range(N)], self.n_units, replace=False)
-        self.map[...] = data[indices].reshape((self.h, self.w, self.m))
+        self.neurons[...] = data[:, indices]
 
         # Algorithm constants
         organizing_iters = 1000
@@ -39,31 +39,32 @@ class SelfOrganizingMap:
         t_2 = 1000
         r_0 = 0.1
         
-        for x in data:
-            # 1) Competitive process: find index of the winning neuron
-            winner_i = np.unravel_index(np.argmax(self.map @ x[:, np.newaxis]), (self.h, self.w))
+        for i in range(N):
+            x = data[:,i][:, np.newaxis]
+            # 1) Competitive process: find coordinate of the winning neuron
+            winner_i = np.unravel_index(np.argmax(x.T @ self.neurons), (self.h, self.w))
+            print("Winner:", winner_i)            
 
             # Self-organizing phase
             for n in range(organizing_iters):
                 # 2) Cooperative process: calculate neighborhood radius
-                # and select neurons within it
-                print("Winner:", winner_i)
+                # and select neurons within it                
                 radius = self.map_radius * np.exp(-n/t_1)
-                distances = np.linalg.norm(self.map_idx - winner_i, axis=2)
+                distances = np.linalg.norm(self.map_pos - winner_i, axis=1)
                 selected = distances <= radius
                 neighborhood = np.exp(-distances[selected]**2 / (2*radius**2))
-
-                # If there are S selected neurons, neighborhood contains S
-                # elements. The next step (equation 9.13) should compute
-                # (x - w) with w being the S selected neurons (accessed using
-                # the selected array). The result is multiplied element-wise
-                # by neighborhood.
                 
-                # 3) Adaptive process: update weights
-                self.map[selected] = 0
-                plt.imshow(np.sum(self.map, axis=2))
-                plt.show()
-                return
+                # 3) Adaptive process: update weights of selected neurons
+                r = r_0 * np.exp(-n/t_2)
+                self.neurons[:, selected] -= r * neighborhood * (self.neurons[:, selected] - x)
+                
+                if n in [1, 10, 50, 100, 250, 500, 999]:
+                    #plt.imshow((np.sum(self.neurons,axis=0)).reshape(self.h, self.w))
+                    plt.figure()
+                    plt.imshow(selected.reshape(self.h, self.w))
+                    plt.title("n = " + str(n))
+                    plt.colorbar()
+                    plt.savefig(str(n))
 
             return
 
@@ -74,6 +75,6 @@ class SelfOrganizingMap:
                 pass
 
 if __name__ == '__main__':
-    som = SelfOrganizingMap(20, 20, 3)
-    data = np.random.randint(0, 9, (1000,3))
+    som = SelfOrganizingMap(30, 30, 3)
+    data = np.random.randint(0, 9, (3,1000))
     som.self_organize(data)
