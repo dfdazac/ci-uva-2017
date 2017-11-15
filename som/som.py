eb0c2db802b589799de62139bd8b1e6cc6ba6dd9
Daniel Daza <dfdazac@gmail.com>
2017-11-15 14:27:30 +0100

    Fixed algorithm and added examples

--- som/som.py
+++ som/som.py
@@ -16,65 +16,55 @@ class SelfOrganizingMap:
         self.neurons = np.empty((m, h * w))
         self.map_pos = np.array([(i,j) for i in range(h) for j in range(w)])
 
-    def self_organize(self, data, monitor=False):
+    def self_organize(self, data):
         """ Carries out self-organization in the SOM using data.
         Args:
             - data: An m by N matrix with data points in the columns.
-        """
-        N = data.shape[1]
-        # Check if data is correct
-        if N < self.n_units:
-            raise ValueError("Insufficient data for self-organization")
+        """        
         if data.shape[0] != self.m:
             raise ValueError("Incorrect data dimensions")
 
         # Initialize neuron weights from unique samples of the data
-        indices = np.random.choice([i for i in range(N)], self.n_units, replace=False)
+        indices = np.random.choice([i for i in range(data.shape[1])], self.n_units, replace=False)
         self.neurons[...] = data[:, indices]
 
-        # Algorithm constants
-        organizing_iters = 1000
-        convergence_iters = 500 * self.n_units
-        t_1 = 1000/np.log(self.map_radius)
-        t_2 = 1000
-        r_0 = 0.1
         
-        for i in range(N):
-            print(i)
+        # Algorithm constants
+        print("Running self-organizing phase...")
+        organizing_iters = 1000
+        r0 = self.map_radius      
+        t1 = organizing_iters/np.log(self.map_radius)
+        t2 = organizing_iters
+        e0 = 0.1
+        self._adapt(data, organizing_iters, r0, t1, t2, e0, "a")
+        
+        print("Running convergence phase...")
+        convergence_iters = 500 * self.n_units
+        r0 = 1
+        t1 = convergence_iters/0.7 # -0.7 = ln(0.5), 0.5 is the final value
+        t2 = convergence_iters/2.3 # -2.3 = ln(0.1), 0.1 is the final value
+        e0 = 0.01
+        self._adapt(data, convergence_iters, r0, t1, t2, e0, "b")        
+
+    def _adapt(self, data, n_iters, r0, t1, t2, e0, title):
+        N = data.shape[1]
+        
+        for n in range(n_iters):
+            # Sample data point
+            i = np.random.randint(0, N)
             x = data[:,i:i+1]
+
             # 1) Competitive process: find coordinate of the winning neuron
-            winner_i = np.unravel_index(np.argmax(x.T @ self.neurons), (self.h, self.w))
-            plt.figure()
-            plt.scatter(self.neurons[0,:], self.neurons[1,:])
-            plt.scatter(data[0,i], data[1,i])
-            plt.title("Before")
-            plt.savefig(str(i) + "a")
-            # Self-organizing phase
-            for n in range(organizing_iters):
-                # 2) Cooperative process: calculate neighborhood radius
-                # and select neurons within it                
-                radius = self.map_radius * np.exp(-n/t_1)
-                distances = np.linalg.norm(self.map_pos - winner_i, axis=1)
-                selected = distances <= radius
-                neighborhood = np.exp(-distances[selected]**2 / (2*radius**2))
-                
-                # 3) Adaptive process: update weights of selected neurons
-                r = r_0 * np.exp(-n/t_2)
-                self.neurons[:, selected] -= r * neighborhood * (self.neurons[:, selected] - x)
+            winner_i = np.unravel_index(np.argmin(np.linalg.norm(self.neurons - x, axis=0)), (self.h, self.w))
 
-            # Convergence phase
-            for n in range(convergence_iters):
-                self.neurons[:, selected] -= 0.01 * neighborhood * (self.neurons[:, selected] - x)
-            plt.figure()
-            plt.scatter(self.neurons[0,:], self.neurons[1,:])
-            plt.scatter(data[0,i], data[1,i])
-            plt.title("After")
-            plt.savefig(str(i) + "b")
-
-            if i == 10:
-                return
-
-if __name__ == '__main__':
-    som = SelfOrganizingMap(20, 20, 3)
-    data = np.random.randint(0, 9, (3,1000))
-    som.self_organize(data)
+            # 2) Cooperative process: calculate neighborhood radius
+            # and select neurons within it                
+            radius = r0 * np.exp(-n/t1)
+            distances = np.linalg.norm(self.map_pos - winner_i, axis=1)
+            selected = distances <= radius
+            neighborhood = np.exp(-distances[selected]**2 / (2*radius**2))
+            
+            # 3) Adaptive process: update weights of selected neurons
+            e = e0 * np.exp(-n/t2)
+            self.neurons[:, selected] -= e * neighborhood * (self.neurons[:, selected] - x)
+        
--- som/som_gauss.py
+++ som/som_gauss.py
@@ -2,22 +2,22 @@ from som import SelfOrganizingMap
 import numpy as np
 import matplotlib.pyplot as plt
 
-mu1 = [0,0]
-mu2 = [0, 2]
-mu3 = [2, 2]
-mu4 = [2, 0]
-sigma = [[0.01, 0], [0, 0.01]]
-n = 100
+mu1 = [-10,-10]
+mu2 = [-10, -10]
+mu3 = [10, 10]
+mu4 = [10, 10]
+sigma = [[0.1, 0], [0, 0.1]]
+n = 400
 
 data = np.concatenate((np.random.multivariate_normal(mu1, sigma, n).T, np.random.multivariate_normal(mu2, sigma, n).T, np.random.multivariate_normal(mu3, sigma, n).T, np.random.multivariate_normal(mu4, sigma, n).T), axis=1)
 data = data[:, np.random.choice([i for i in range(data.shape[1])], data.shape[1],replace=False)]
-print(data.shape)
-plt.scatter(data[0,:], data[1,:])
-plt.show()
+#print(data.shape)
+#plt.scatter(data[0,:], data[1,:])
+#plt.show()
 
-som = SelfOrganizingMap(10, 10, 2)
-print(som.neurons)
+som = SelfOrganizingMap(15, 15, 2)
+#print(som.neurons)
 som.self_organize(data)
-print(som.neurons)
+#print(som.neurons)
 plt.scatter(som.neurons[0,:], som.neurons[1,:])
 plt.show()
